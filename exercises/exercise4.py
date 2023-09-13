a9de428232c9ca0b2bc67ea115220d36fbc65c2e
hmendoza711 <hectormendoza4@hotmail.com>
2023-09-13 03:24:27 -0300

    EXERCISE 4

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -13,7 +13,26 @@ def es_vocal_if(letra: str) -> bool:
 
     Referencia: https://docs.python.org/3/library/stdtypes.html#string-methods
     """
+    letra = letra.lower()
 
+    if letra == 'a':
+        return True
+    if letra == 'e':
+        return True
+    if letra == 'i':
+        return True
+    if letra == 'o':
+        return True
+    if letra == 'u':
+        return True
+    return False
+
+
+print(f"Es vocal: {es_vocal_if('a')}")
+print(f"Es vocal: {es_vocal_if('b')}")
+print(f"Es vocal: {es_vocal_if('A')}")
+print(f"Es vocal: {es_vocal_if('e')}")
+print(f"Es vocal: {es_vocal_if('E')}")
 
 # NO MODIFICAR - INICIO
 assert es_vocal_if("a")
@@ -23,7 +42,7 @@ assert es_vocal_if("e")
 assert es_vocal_if("E")
 # NO MODIFICAR - FIN
 
-
+print("######################################")
 ###############################################################################
 
 
@@ -39,7 +58,14 @@ def es_vocal_if_in(letra: str) -> bool:
 
     Referencia: https://docs.python.org/3/reference/expressions.html#membership-test-operations # noqa: E501
     """
-
+    vocales = list('aeiou')
+    if letra.lower() in vocales:
+        return True
+    return False
+  
+print(f"Es vocal: {es_vocal_if_in('a')}")
+print(f"Es vocal: {es_vocal_if_in('b')}")
+print(f"Es vocal: {es_vocal_if_in('A')}")
 
 # NO MODIFICAR - INICIO
 assert es_vocal_if_in("a")
@@ -47,7 +73,7 @@ assert not es_vocal_if_in("b")
 assert es_vocal_if_in("A")
 # NO MODIFICAR - FIN
 
-
+print("######################################")
 ###############################################################################
 
 
@@ -60,7 +86,12 @@ def es_vocal_in(letra: str) -> bool:
         - No utilizar FOR.
         - No utilizar listas.
     """
+    vocales = list('aeiou')
+    return letra.lower() in vocales 
 
+print(f"Es vocal: {es_vocal_in('a')}")
+print(f"Es vocal: {es_vocal_in('b')}")
+print(f"Es vocal: {es_vocal_in('A')}")   
 
 # NO MODIFICAR - INICIO
 assert es_vocal_in("a")
