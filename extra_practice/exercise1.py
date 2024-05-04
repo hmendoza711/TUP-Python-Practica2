1925efaab45f70b3cc541a8afd290f5fe179a8b4
Hector Mendoza <hectormaximendoza@gmail.com>
2024-05-04 17:11:12 -0300

    exercise1

--- extra_practice/exercise1.py
+++ extra_practice/exercise1.py
@@ -1,13 +1,13 @@
 # Escribe un programa que pida al usuario ingresar su edad y luego imprima un mensaje indicando si es mayor de edad o no.
 
 while True:
-    edad = int(input("Ingrese su edad: "))
-
+    edad = int(input("Ingrese su edad:\n"))
     if edad >= 18:
-        print("SOS MAYOR DE EDAD!!")
+        print("SOS MAYOR DE EDAD")
     else:
-        print("SOS MENOR DE EDAD!!")
+        print("SOS MENOR DE EDAD")
     
     opt = input("Desea ingresar otra edad?? (s/n)\n")
+    
     if opt.lower() != 's':
         break
---
+++ extra_practice/exercise2.py
@@ -0,0 +1,2 @@
+#Escribe un programa que tome una lista de numeros y calcule la suma de todos los elementos.
+
