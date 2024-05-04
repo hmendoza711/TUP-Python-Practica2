ac413d25ac8d0c15f30f4ae87129249a24be634b
Hector Mendoza <hectormaximendoza@gmail.com>
2024-05-04 19:43:44 -0300

    exercise2

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -1,7 +1,6 @@
 """Comparaciones Encadenadas, Cantidad Arbitraria de Parámetros, Recursividad.
 """
 
-
 def maximo_encadenado(a: float, b: float, c: float) -> float:
     """Toma 3 números y devuelve el máximo.
 
@@ -13,22 +12,23 @@ def maximo_encadenado(a: float, b: float, c: float) -> float:
 
     Referencia: https://docs.python.org/3/reference/expressions.html#comparisons # noqa: E501
     """
-    if a<b>c:
-        return b
-    if a<c>b:
-        return c
-    if b<a>c:
-        return a
     
-    # if c<a>b:
-    #     return a
-
-    # if a<b>c:
-    #     return b
-
-    # return c
+    if c < a > b:
+        return a
+    if a < b > c:
+        return b
+    return c
+    
 print(f"Numero maximo: {maximo_encadenado(1,10,5)}")
+print(f"Numero maximo: {maximo_encadenado(5, 10, 1)}")
+print(f"Numero maximo: {maximo_encadenado(5, 10, 5)}")
+print("###############################################")
+print(f"Numero maximo: {maximo_encadenado(4, 9, 18)}")
 print(f"Numero maximo: {maximo_encadenado(9, 4, 18)}")
+print(f"Numero maximo: {maximo_encadenado(9, 9, 18)}")
+print("###############################################")
+print(f"Numero maximo: {maximo_encadenado(24, 9, 18)}")
+print(f"Numero maximo: {maximo_encadenado(24, 18, 9)}")
 print(f"Numero maximo: {maximo_encadenado(24, 18, 18)}")
 
 # # NO MODIFICAR - INICIO
@@ -56,7 +56,7 @@ def maximo_cuadruple(a: float, b: float, c: float, d: float) -> float:
     Referencia: https://docs.python.org/3/library/functions.html#max"""
 
     return max([a,b,c,d])
-   
+
 print(f"Numero maximo cuadruple: {maximo_cuadruple(1, 10, 5, -5)}")
 print(f"Numero maximo cuadruple: {maximo_cuadruple(4, 9, 18, 6)}")
 print(f"Numero maximo cuadruple: {maximo_cuadruple(24, 9, 18, 20)}")
