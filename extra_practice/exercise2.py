9d7d83af1312d62046463de6200dfc2e89ae960c
Hector Mendoza <hectormaximendoza@gmail.com>
2024-05-04 17:26:52 -0300

    exercise2

--- extra_practice/exercise2.py
+++ extra_practice/exercise2.py
@@ -1,2 +1,10 @@
 #Escribe un programa que tome una lista de numeros y calcule la suma de todos los elementos.
 
+numbers = [10,20,30,40,50]
+
+a,b,c,d,e = numbers
+
+total =  a + b + c + d + e
+
+print(f"El total es: {total}")
+
